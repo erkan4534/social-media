2b03111b93a980042fe39a64f2d593e4fdfdeb1f
Erkan Yıldırım <erkanyildirim28@gmail.com>
2024-03-19 21:36:53 +0300

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 function App() {
   return (
