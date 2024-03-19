9c603d81a0aceb1bc7a73710b27820ba21a2b006
Erkan Yıldırım <erkanyildirim28@gmail.com>
2024-03-19 22:05:57 +0300

    login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,19 @@
-const Login = (props) => {
+const Login = () => {
   return (
-    <div>
-      <div></div>
-      <div></div>
+    <div className="flex ">
+      <div className="flex-1 h-dvh">
+        <img
+          src="https://images.unsplash.com/photo-1710104434504-0261d06fa832?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          className="object-cover w-full h-full"
+        />
+      </div>
+      <div className="flex-1">
+        <form className="flex flex-col">
+          <input placeholder="Email" type="email" />
+          <input placeholder="Password" type="email" />
+          <button>Log In</button>
+        </form>
+      </div>
     </div>
   );
 };
