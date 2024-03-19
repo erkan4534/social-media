be5883b6a110989f4be0457181d56312749a208a
Erkan Yıldırım <erkanyildirim28@gmail.com>
2024-03-19 22:20:31 +0300

    login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,8 @@
+import "./Login.css";
+
 const Login = () => {
   return (
-    <div className="flex ">
+    <div className="flex">
       <div className="flex-1 h-dvh">
         <img
           src="https://images.unsplash.com/photo-1710104434504-0261d06fa832?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -8,7 +10,7 @@ const Login = () => {
         />
       </div>
       <div className="flex-1">
-        <form className="flex flex-col">
+        <form className="loginInput">
           <input placeholder="Email" type="email" />
           <input placeholder="Password" type="email" />
           <button>Log In</button>
