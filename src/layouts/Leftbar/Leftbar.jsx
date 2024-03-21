a89600a1bd63b166e6c04109171b9f60124aa46b
Erkan Yıldırım <erkanyildirim28@gmail.com>
2024-03-21 18:37:03 +0300

    leftbar css

--- src/layouts/Leftbar/Leftbar.jsx
+++ src/layouts/Leftbar/Leftbar.jsx
@@ -3,7 +3,7 @@ import CloseFriend from "../../pages/CloseFriend/CloseFriend";
 const Leftbar = () => {
   return (
     <div>
-      <div className="mt-10">
+      <div className="mt-10 ml-5">
         <CloseFriend />
       </div>
     </div>
