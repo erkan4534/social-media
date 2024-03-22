02fc9c07bf10003505babaa245cb9a6ac9f97a25
Erkan Yıldırım <erkanyildirim28@gmail.com>
2024-03-22 20:56:57 +0300

    new account

--- src/pages/NewAccount/NewAccount.jsx
+++ src/pages/NewAccount/NewAccount.jsx
@@ -12,20 +12,21 @@ const NewAccount = ({ showLogin }) => {
   return (
     <div className="newAccountForm">
       <form onSubmit={onNewAccountSubmit} noValidate>
-        {inputDataArray.map((inputData) => (
-          <div key={inputData.id} className="flex flex-col gap-2">
-            <input
-              placeholder={inputData.placeholder}
-              type={inputData.type}
-              name={inputData.name}
-              className="newAccountInput"
-              // onChange={handleChange}
-              required={inputData.required}
-            />
-            {/* {emailValid && <span>It should be a valid email address!</span>} */}
-          </div>
-        ))}
-
+        <div key={inputData.id} className="flex flex-col gap-2">
+            {inputDataArray.map((inputData) => (
+            
+                <input
+                placeholder={inputData.placeholder}
+                type={inputData.type}
+                name={inputData.name}
+                className="newAccountInput"
+                // onChange={handleChange}
+                required={inputData.required}
+                />
+                {/* {emailValid && <span>It should be a valid email address!</span>} */}
+            
+            ))}
+        </div>
         <div className="mt-3 flex justify-between">
           <button className="newAccountButton" onClick={showLogin}>
             Back To newAccount
