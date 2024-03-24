61831bb3342452a628d50e729074ee4af3c280f6
Erkan Yıldırım <erkanyildirim28@gmail.com>
2024-03-24 11:51:10 +0300

    add info userData

--- src/data/userData.js
+++ src/data/userData.js
@@ -5,6 +5,7 @@ export const userData = [
     surname: "Kaya",
     password: "123",
     email: "erkan1.yildirim@gmail.com",
+    profilePicture: "assets/person/1.jpeg",
   },
   {
     id: 2,
@@ -12,6 +13,7 @@ export const userData = [
     surname: "Durmaz",
     password: "123",
     email: "erkan2.yildirim@gmail.com",
+    profilePicture: "assets/person/2.jpeg",
   },
 
   {
@@ -20,6 +22,7 @@ export const userData = [
     surname: "Dayi",
     password: "123",
     email: "erkan3.yildirim@gmail.com",
+    profilePicture: "assets/person/3.jpeg",
   },
 
   {
@@ -28,5 +31,60 @@ export const userData = [
     surname: "Bayraktar",
     password: "123",
     email: "erkan4.yildirim@gmail.com",
+    profilePicture: "assets/person/4.jpeg",
+  },
+
+  {
+    id: 5,
+    ad: "Rasim",
+    surname: "Dayi",
+    password: "123",
+    email: "erkan5.yildirim@gmail.com",
+    profilePicture: "assets/person/5.jpeg",
+  },
+
+  {
+    id: 6,
+    ad: "Ezel",
+    surname: "Bayraktar",
+    password: "123",
+    email: "erkan6.yildirim@gmail.com",
+    profilePicture: "assets/person/6.jpeg",
+  },
+
+  {
+    id: 7,
+    ad: "Ezel",
+    surname: "Bayraktar",
+    password: "123",
+    email: "erkan7.yildirim@gmail.com",
+    profilePicture: "assets/person/7.jpeg",
+  },
+
+  {
+    id: 8,
+    ad: "Ezel",
+    surname: "Bayraktar",
+    password: "123",
+    email: "erkan8.yildirim@gmail.com",
+    profilePicture: "assets/person/8.jpeg",
+  },
+
+  {
+    id: 9,
+    ad: "Ezel",
+    surname: "Bayraktar",
+    password: "123",
+    email: "erkan9.yildirim@gmail.com",
+    profilePicture: "assets/person/9.jpeg",
+  },
+
+  {
+    id: 10,
+    ad: "Ezel",
+    surname: "Bayraktar",
+    password: "123",
+    email: "erkan8.yildirim@gmail.com",
+    profilePicture: "assets/person/10.jpeg",
   },
 ];
