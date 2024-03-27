a3c86efb62bd961dba6a64a8fa219c602497df15
Erkan Yıldırım <erkanyildirim28@gmail.com>
2024-03-27 17:45:14 +0300

    remove button

--- src/pages/Friend/Friend.jsx
+++ src/pages/Friend/Friend.jsx
@@ -1,16 +1,23 @@
 import { useSelector } from "react-redux";
+import "./Friend.css";
 const Friend = () => {
   const { user } = useSelector((state) => state.auth);
 
   return (
     <div>
-      {user &&
-        user.firends &&
-        user.firends.map((firend) => <div key={firend.id}>{firend.name}</div>)}
+      {user.firends.map((friend) => (
+        <div key={friend.id} className="friendList">
+          <div className="friend">
+            <img src={friend.profilePicture} />
+            <span>
+              {friend.name} {friend.surname}
+            </span>
+          </div>
+          <button className="removeFriendButton">Remove</button>
+        </div>
+      ))}
     </div>
   );
 };
-
-Friend.propTypes = {};
 
 export default Friend;
