5359abf481b254dd9868557b2f72c6da691c14b2
Erkan Yıldırım <erkanyildirim28@gmail.com>
2024-03-30 18:23:30 +0300

    comment

--- src/pages/Share/Share.jsx
+++ src/pages/Share/Share.jsx
@@ -156,6 +156,7 @@ const Share = () => {
                   <TextArea
                     name="commentTextArea"
                     onChange={commentChange}
+                    value={comment}
                   ></TextArea>
                   <button onClick={() => postCommnet()}>Post</button>
                 </div>
