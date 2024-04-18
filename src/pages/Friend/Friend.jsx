f1e9916fa10017359035a76f36e829c4f928322d
Erkan Yıldırım <erkanyildirim28@gmail.com>
2024-04-18 06:27:00 +0300

    Friend

--- src/pages/Friend/Friend.jsx
+++ src/pages/Friend/Friend.jsx
@@ -4,49 +4,72 @@ import { removeFriend } from "../../redux/action/authActions";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import SearchIcon from "@mui/icons-material/Search";
+import { useState } from "react";
+
 const Friend = ({ userInfo, userDataArray }) => {
   const dispatch = useDispatch();
+  const [searchFriendTerm, setSearchFriendTerm] = useState("");
   const { user } = useSelector((state) => state.auth);
+
+  const handleSearchChange = (event) => {
+    setSearchFriendTerm(event.target.value.toLowerCase());
+  };
+
+  if (searchFriendTerm) {
+    userDataArray = userDataArray.filter((member) =>
+      `${member.name} ${member.surname}`
+        .toLowerCase()
+        .includes(searchFriendTerm)
+    );
+  }
+
+  let friendsArray = userDataArray.filter((user) =>
+    userInfo.friends.includes(user.id)
+  );
+
   return (
     <div>
-      <div className="friendTitle">
-        <span>Friends</span>
+      <div className="searchFriendBar">
+        <SearchIcon className="searchIconFriend" />
+        <input
+          className="searchFriendInput"
+          onChange={handleSearchChange}
+          value={searchFriendTerm}
+          placeholder="Search friends"
+        />
       </div>
-      <div className="friendsContainer">
-        {userInfo &&
-          userInfo.friends &&
-          userInfo.friends.map((friendId) => {
-            const findUser = userDataArray.find(
-              (usr) => usr.id === Number(friendId)
-            );
-            return (
-              <div key={friendId} className="friendList">
-                <div className="friend">
-                  <Link to={`/profile/${friendId}`} className="linkFriend">
-                    <img
-                      src={findUser.profilePicture}
-                      alt={`${findUser.name} ${findUser.surname}`}
-                    />
-                    <span className="mt-1">
-                      {findUser.name} {findUser.surname}
-                    </span>
-                  </Link>
-                </div>
-                {user?.id === userInfo.id && (
-                  <button
-                    className="removeFriendButton"
-                    onClick={() => dispatch(removeFriend(friendId, userInfo))}
-                  >
-                    UnFollowed
-                  </button>
-                )}
 
-                {user?.id !== userInfo.id && (
-                  <span className="followed-text-for-friend">Followed</span>
-                )}
+      <div className="friendsContainer">
+        {friendsArray.map((friend) => {
+          return (
+            <div key={friend.id} className="friendList">
+              <div className="friend">
+                <Link to={`/profile/${friend.id}`} className="linkFriend">
+                  <img
+                    src={friend.profilePicture}
+                    alt={`${friend.name} ${friend.surname}`}
+                  />
+                  <span className="mt-1">
+                    {friend.name} {friend.surname}
+                  </span>
+                </Link>
               </div>
-            );
-          })}
+              {user?.id === userInfo.id && (
+                <button
+                  className="removeFriendButton"
+                  onClick={() => dispatch(removeFriend(friend, userInfo))}
+                >
+                  UnFollowed
+                </button>
+              )}
+
+              {user?.id !== userInfo.id && (
+                <span className="followed-text-for-friend">Followed</span>
+              )}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
