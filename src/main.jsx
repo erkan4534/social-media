9d3ec6cd2040029e5396aaff397210ac13a2c094
Erkan Yıldırım <erkanyildirim28@gmail.com>
2024-04-26 02:20:46 +0300

    redux toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import { Provider } from "react-redux";
 //import AuthProvider from "./context/AuthProvider.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-import store from "./redux/store.js";
+//import store from "./redux/store.js";
+import store from "./redux-toolkit/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <AuthProvider>
