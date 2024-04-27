cafdb933fc9c6984ffedcbd853673d9ca485c8cb
Erkan Yıldırım <erkanyildirim28@gmail.com>
2024-04-27 13:41:45 +0300

    app scroller

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
 import "./App.css";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  useLocation,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import { MainRoutes } from "./routes/MainRoutes";
 import { AdminRoutes } from "./routes/AdminRoutes";
-import { useEffect } from "react";
 
 function App() {
   const router = createBrowserRouter([...MainRoutes, ...AdminRoutes]);
